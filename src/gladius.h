491cd12c0a9f625f1560430c2dca478020377465
pithecantrope <fartdraft@gmail.com>
2026-03-19 22:11:38 +0300

    Full circle

--- src/gladius.h
+++ src/gladius.h
@@ -11,18 +11,12 @@
  *              #include "gladius.h"
  *      Elsewhere:
  *              #include "gladius.h"
- *
- * Note:
- *      - Panics on failure by default. No need to check return values.
- *      - Short aliases by default. Define GLADIUS_PREFIXED to disable.
 */
 
 #ifndef GLADIUS_HEADER
 #define GLADIUS_HEADER
 
-#if defined(__cplusplus)
-#error "Gladius does not support C++."
-#elif (!defined(__STDC_VERSION__) || (__STDC_VERSION__ < 202000L))
+#if !(defined(__STDC_VERSION__) && (__STDC_VERSION__ >= 202000L))
 #error "Gladius requires C23 or later."
 #endif
 
@@ -32,157 +26,25 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-#ifndef GLD_API
-#define GLD_API extern
-#endif // GLD_API
+#ifndef API
+#define API extern
+#endif // API
 
-#ifndef GLD_MALLOC
-#define GLD_MALLOC(size) malloc(size)
-#endif // GLD_MALLOC
+#ifndef MALLOC
+#define MALLOC(size) malloc(size)
+#endif // MALLOC
 
-#ifndef GLD_FREE
-#define GLD_FREE(ptr) free(ptr)
-#endif // GLD_FREE
+#ifndef FREE
+#define FREE(ptr) free(ptr)
+#endif // FREE
 
-#ifdef NDEBUG
-#define GLD_PANIC(cond, msg)                                                                       \
-        do {                                                                                       \
-                if (cond)                                                                          \
-                        unreachable();                                                             \
-        } while (0)
-#else
-#define GLD_PANIC(cond, msg)                                                                       \
-        do {                                                                                       \
-                if (cond) {                                                                        \
-                        fprintf(stderr,                                                            \
-                                "GLD_PANIC: " msg "\n"                                             \
-                                "File \"%s\", line %d, in %s\n"                                    \
-                                "\t" #cond "\n",                                                   \
-                                __FILE__, __LINE__, __func__);                                     \
-                        exit(EXIT_FAILURE);                                                        \
-                }                                                                                  \
-        } while (0)
-#endif // NDEBUG
-
-#define GLD_CONCAT_(left, right) left##right
-#define GLD_CONCAT(left, right)  GLD_CONCAT_(left, right)
-#define GLD_UNIQUE(name)         GLD_CONCAT(name, __LINE__)
+#define CONCAT_(left, right) left##right
+#define CONCAT(left, right)  CONCAT_(left, right)
+#define UNIQUE(name)         CONCAT(name, __LINE__)
 
 // Arena -------------------------------------------------------------------------------------------
 
-// Linear allocator with fixed capacity
-typedef struct {
-        size_t cap;
-        size_t* len;
-} GldArena;
-
-[[nodiscard]] GLD_API bool gld_arena_valid(GldArena a);
-GLD_API void gld_arena_println(GldArena a);
-
-#define GLD_KiB(x) ((size_t)(x) << 10)
-#define GLD_MiB(x) ((size_t)(x) << 20)
-#define GLD_GiB(x) ((size_t)(x) << 30)
-
-[[nodiscard]] GLD_API GldArena gld_arena_alloc(size_t capacity);
-GLD_API void gld_arena_reset(GldArena a);
-GLD_API void gld_arena_free(GldArena a);
-
-// Zero count is valid
-[[nodiscard]] GLD_API void* gld_arena_new(GldArena a, size_t count, size_t size, size_t align);
-#define GLD_NEW(a, type, num) (type*)gld_arena_new(a, num, sizeof(type), alignof(type))
-#define GLD_SCOPE(a)                                                                               \
-        for (size_t GLD_UNIQUE(len) = *a.len, GLD_UNIQUE(once) = 1; GLD_UNIQUE(once);              \
-             GLD_UNIQUE(once) = 0, *a.len = GLD_UNIQUE(len))                                       \
-                for (int GLD_UNIQUE(break) = 1; GLD_UNIQUE(break); GLD_UNIQUE(break) = 0)
-
-[[nodiscard]] GLD_API bool gld_arena_contains(GldArena a, void* ptr);
-[[nodiscard]] GLD_API bool gld_arena_newest(GldArena a, void* ptr, size_t size);
-
-#ifndef GLADIUS_PREFIXED
-#define Arena          GldArena
-#define arena_valid    gld_arena_valid
-#define arena_println  gld_arena_println
-#define KiB            GLD_KiB
-#define MiB            GLD_MiB
-#define GiB            GLD_GiB
-#define arena_alloc    gld_arena_alloc
-#define arena_reset    gld_arena_reset
-#define arena_free     gld_arena_free
-#define arena_new      gld_arena_new
-#define NEW            GLD_NEW
-#define SCOPE          GLD_SCOPE
-#define arena_contains gld_arena_contains
-#define arena_newest   gld_arena_newest
-#endif // GLADIUS_PREFIXED
-
 #ifdef GLADIUS_IMPLEMENTATION
-bool
-gld_arena_valid(GldArena a) {
-        return a.cap > 0 && a.len != nullptr && *a.len <= a.cap;
-}
-
-void
-gld_arena_println(GldArena a) {
-        assert(gld_arena_valid(a) && "Invalid GldArena");
-        printf("GldArena{cap:%zu, len:%zu}\n", a.cap, *a.len);
-}
-
-GldArena
-gld_arena_alloc(size_t capacity) {
-        assert(capacity > 0 && "Invalid capacity");
-        assert(capacity <= SIZE_MAX - sizeof(size_t) && "Invalid capacity");
-        size_t total = sizeof(size_t) + capacity;
-        GldArena a = {
-                .cap = capacity,
-                .len = GLD_MALLOC(total),
-        };
-        GLD_PANIC(a.len == nullptr, "GLD_MALLOC failed");
-        *a.len = 0;
-        return a;
-}
-
-void
-gld_arena_reset(GldArena a) {
-        assert(gld_arena_valid(a) && "Invalid GldArena");
-        *a.len = 0;
-}
-
-void
-gld_arena_free(GldArena a) {
-        assert(gld_arena_valid(a) && "Invalid GldArena");
-        *a.len = SIZE_MAX;
-        GLD_FREE(a.len);
-}
-
-void*
-gld_arena_new(GldArena a, size_t count, size_t size, size_t align) {
-        assert(gld_arena_valid(a) && "Invalid GldArena");
-        assert(size > 0 && "Invalid size");
-        assert(align > 0 && (align & (align - 1)) == 0 && "Invalid alignment");
-
-        char* mem = (char*)a.len + sizeof(size_t);
-        size_t padding = -(uintptr_t)(mem + *a.len) & (align - 1);
-        assert(a.cap - *a.len >= padding && "Increase GldArena capacity");
-        assert(count <= (a.cap - *a.len - padding) / size && "Increase GldArena capacity");
-        void* ptr = mem + *a.len + padding;
-        *a.len += padding + count * size;
-        return ptr;
-}
-
-bool
-gld_arena_contains(GldArena a, void* ptr) {
-        assert(gld_arena_valid(a) && "Invalid GldArena");
-        assert(ptr != nullptr && "Invalid pointer");
-        char* mem = (char*)a.len + sizeof(size_t);
-        return mem <= (char*)ptr && (char*)ptr <= mem + *a.len;
-}
-
-bool
-gld_arena_newest(GldArena a, void* ptr, size_t size) {
-        assert(gld_arena_contains(a, ptr) && "Invalid pointer");
-        char* mem = (char*)a.len + sizeof(size_t);
-        return mem + *a.len == (char*)ptr + size;
-}
 #endif // GLADIUS_IMPLEMENTATION
 
 // String ------------------------------------------------------------------------------------------
--- src/test.c
+++ src/test.c
@@ -1,53 +1,14 @@
-// #define NDEBUG
-// #define GLD_API static inline
-// #define GLD_MALLOC(size) custom_malloc(size)
-// #define GLD_FREE(ptr)    custom_free(ptr)
-// TODO: #define GLADIUS_PREFIXED
+// #define API static inline
+// #define MALLOC(size) custom_malloc(size)
+// #define FREE(ptr)    custom_free(ptr)
 #define GLADIUS_IMPLEMENTATION
 #include "gladius.h"
 
 #define TEST(name)       static void test_##name(Arena a)
-#define check(condition) GLD_PANIC(!(condition), "Test failed")
-static constexpr size_t ARENA_CAPACITY = MiB(1);
-
-TEST(arena_reset) {
-        *a.len += ARENA_CAPACITY;
-        arena_reset(a);
-        check(*a.len == 0);
-}
-
-TEST(arena_new) {
-        check(NEW(a, long, 0) != nullptr);
-        uint8_t* arr = NEW(a, uint8_t, 3);
-        arr[0] = arr[1] = arr[2] = 7;
-        (void)NEW(a, int32_t, 1);
-        check(*arr == 7 && *a.len == 8);
-}
-
-TEST(arena_scope) {
-        size_t before = *a.len;
-        SCOPE(a) {
-                (void)NEW(a, long, 2);
-                break;
-        }
-        check(before == *a.len);
-}
-
-TEST(arena) {
-        check(a.cap == ARENA_CAPACITY);
-        test_arena_reset(a);
-        test_arena_new(a);
-        test_arena_scope(a);
-}
+#define check(condition) assert((condition) && "Test failed")
 
 int
 main(void) {
-        Arena a = arena_alloc(ARENA_CAPACITY);
-
-        test_arena(a);
-        arena_println(a);
-
         puts("\nSuccess!");
-        arena_free(a);
         return 0;
 }
